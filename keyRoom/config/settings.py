eda6f0fa38b4c73bb9a10749a68e76f2ed539484
fabiomoraiss <rarinaldo@outlook.com>
2025-10-23 17:42:40 -0300

    implemetado a funcionalidade de login para uma pagina protegida

--- keyRoom/config/settings.py
+++ keyRoom/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login_page',
+    'user',
+    'core'
 ]
 
 MIDDLEWARE = [
--- keyRoom/login_page/urls.py
+++ keyRoom/login_page/urls.py
@@ -4,8 +4,8 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path("login", views.login, name="login_page"),
-    path("signup", views.signup, name="signup_page"),
+    path("login/", views.login, name="login_page"),
+    path("signup/", views.signup, name="signup_page"),
     path("", RedirectView.as_view(pattern_name="login_page"), name="home_page"),
     
 
